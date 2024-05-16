bcde7794ae3fb2852c932b29e97049e99fc9fc9f
Trying to be DEV <88807257+katiaLins@users.noreply.github.com>
2024-05-15 23:23:06 -0300

    Add files via upload

--- installvoid.sh
+++ installvoid.sh
@@ -33,13 +33,15 @@ case "$installChoice" in
         echo "Installing utilities and system tools 2..."
         sudo sudo xbps-install -Rs xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils gnome-browser-connector nautilus
 
+        sudo reboot
+
         echo "Installing additional applications..."
         sudo xbps-install -S neofetch btop grub-customizer gvfs-mtp gvfs ntfs-3g mpv firefox wget git xz unzip zip nano vim gptfdisk xtools mtools mlocate 
 
         sudo xbps-install -S fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi utoconf automake bison m4 make libtool flex meson ninja optipng sassc
         
         echo "Installig audio"
-        sudo xbps-install pulseaudio pulseaudio-utils pulsemixer alsa-plugins-pulseaudio bluez
+        sudo xbps-install pulseaudio pulseaudio-utils pulsemixer alsa-plugins-pulseaudio bluez gnome-tweaks
 
         echo "Install LibreOffice:"
         sudo xbps-install libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw libreoffice-math libreoffice-base libreoffice-gnome libreoffice-i18n-pt-BR
