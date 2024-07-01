82c90b7e28a4685927a5c8a1022a31f7d7c99d7e
Trying to be DEV <88807257+katiaLins@users.noreply.github.com>
2024-07-01 13:46:40 -0300

    Add files via upload

--- installvoid.sh
+++ installvoid.sh
@@ -11,7 +11,7 @@ case "$installChoice" in
     case "$upgradeChoice" in
       y/Y/yes/Yes/YES )
         echo "Upgrading system..."
-        sudo xbps-install -Syu
+        sudo xbps-install -Syuv
       ;;
       n/N/no/No/NO )
         echo "Skipping system upgrade."
@@ -25,23 +25,18 @@ case "$installChoice" in
         sudo xbps-install -Rs void-repo-nonfree    
 
         echo "Installing Video..."  
-        sudo xbps-install -S xf86-video-vmware xf86-video-vesa mesa wayland wayland-protocols wayland-utils dbus avahi curl         
+        sudo xbps-install -S xf86-video-vmware xf86-video-vesa mesa xorg xorg-fonts xinit dbus avahi curl i3block i3 i2status xdg-user-dirs       
 
         echo "Installing utilities and system tools..."
-        sudo xbps-install -S gdm gnome-shell gnome-backgrounds gnome-terminal gnome-control-center gnome-settings-daemon gnome-shell-extensions gnome-terminal 
-
-        echo "Installing utilities and system tools 2..."
-        sudo sudo xbps-install -Rs xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils gnome-browser-connector nautilus
-
-        sudo reboot
+        sudo xbps-install -S lightdm lightdm-gtk-greeter xfce4-clipman-plugin Thunar thunar-volman thunar-archive-plugin file-roller volumeicon scrot xarchiver
 
         echo "Installing additional applications..."
-        sudo xbps-install -S neofetch btop grub-customizer gvfs-mtp gvfs ntfs-3g mpv firefox wget git xz unzip zip nano vim gptfdisk xtools mtools mlocate 
+        sudo xbps-install -S neofetch grub-customizer gvfs-mtp gvfs ntfs-3g mpv firefox wget git xz unzip zip gptfdisk xtools mtools mlocate zsh kitty 
 
-        sudo xbps-install -S fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi utoconf automake bison m4 make libtool flex meson ninja optipng sassc
+        sudo xbps-install -S fuse-exfat bash-completion linux-headers  ffmpeg mesa-vdpau mesa-vaapi utoconf make mc nerd-fonts dejavu-fonts-ttf  pasystray
         
         echo "Installig audio"
-        sudo xbps-install pulseaudio pulseaudio-utils pulsemixer alsa-plugins-pulseaudio bluez gnome-tweaks
+        sudo xbps-install pulseaudio pulseaudio-utils pulsemixer alsa-plugins-pulseaudio bluez polkit polkit-gnome lxappearance nitrgen axel gnome-themes-standart
 
         echo "Install LibreOffice:"
         sudo xbps-install libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw libreoffice-math libreoffice-base libreoffice-gnome libreoffice-i18n-pt-BR
@@ -50,10 +45,10 @@ case "$installChoice" in
         sudo sudo xbps-install -S flatpak
 
         echo "Installing Screenshot Utility..."
-        sudo xbps-install NetworkManager
+        sudo xbps-install NetworkManager network-manager-applet
 
         echo "Starting services..."
-        sudo ln -s /etc/sv/gdm /var/service/
+        sudo ln -s /etc/sv/lightdm /var/service/
         sudo ln -s /etc/sv/NetworkManager /var/service/
         sudo ln -sv /etc/sv/bluetoothd /var/service
         sudo ln -s /etc/sv/dbus /var/service/
